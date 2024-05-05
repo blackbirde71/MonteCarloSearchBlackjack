2dfdf04f0ef6ea6dd66fc6d0abf073ec37ac2650
cjw3 <cjw3@williams.edu>
2024-05-05 17:14:32 -0400

    Create Repository

---
+++ Blackjack.java
@@ -0,0 +1,3 @@
+/*
+Describes a game of blackjack between a player and a computer (which will ultimately be an instance of MonteCarloSearchTree in our implementation)
+*/
---
+++ Play.java
@@ -0,0 +1,6 @@
+/*
+Main file. Covers the end user implementation, including features like the command line interface implementation
+Will instantiate a Blackjack game with a MonteCarloSearchTree as its computer:
+	game = new Blackjack();
+	game.computer = new MonteCarloSearchTree();
+*/

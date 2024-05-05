8e80ab0a3a7f2bb87b3cd2fc3db61f3bafe11a10
cjw3 <cjw3@williams.edu>
2024-05-05 19:49:32 -0400

    add notes

--- mcsblackjack/PlayBlackjack.java
+++ mcsblackjack/PlayBlackjack.java
@@ -1,6 +1,6 @@
 /*
 * Main file. Will manage:
-	- Blackjack player vs computer game
+	- Blackjack player   vs computer game
 	- Command line interface
 * Will instantiate a Blackjack game with a MonteCarloSearchTree as its computer:
 * 	game = new Blackjack();

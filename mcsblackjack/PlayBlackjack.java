b3c2fdc6092c11e68f5b3d1a83a7bd3fabc672dc
812-gCo <159477127+812-gCo@users.noreply.github.com>
2024-05-05 20:09:23 -0400

    fix PlayBlackjack.java description

--- mcsblackjack/PlayBlackjack.java
+++ mcsblackjack/PlayBlackjack.java
@@ -2,8 +2,5 @@
 * Main file. Will manage:
 	- Blackjack player   vs computer game
 	- Command line interface
-* Will instantiate a Blackjack game with a MonteCarloSearchTree as its computer:
-* 	game = new Blackjack();
-*	game.computer = new MonteCarloSearchTree();
 */
 package mcsblackjack;

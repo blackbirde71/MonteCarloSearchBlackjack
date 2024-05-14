166755857a07a0fa73f3f757a4b0438353a42b28
ar21 <ar21@williams.edu>
2024-05-14 12:58:08 -0400

    comments clarification

--- mcsblackjack/BlackjackTree.java
+++ mcsblackjack/BlackjackTree.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class BlackjackTree extends MonteCarloTree<BlackjackState> {
 
+	// the first card of the dealer is visible, so it should be
+	// account for in simulations
 	public int dealerCard;
 
 	BlackjackTree(int dealerCard, ArrayList<Integer> initCards, int EXPLORATION, int NUMITERATIONS) {
@@ -20,13 +22,18 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
             newReward += rollout(current.state);
         }
 
+
+        // reward is weighted, i.e. the average of all rollouts
         newReward /= NUMITERATIONS;
 
         double oldReward = current.reward;
 
-        //type of backprop:
+        // type of backprop (enum)
         BackPropType bct;
 
+        // update() is only called from leaf nodes
+        // so it does not deal with mixed sources of reward: standing or hitting
+        // so no need to call max()
         if (current.state.isStanding) {
             current.rewardStand += newReward;
             current.reward += newReward;
@@ -50,7 +57,7 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
 		b[52] = new BlackjackState(state.cards, true);
 
 		for (int i=0; i<52; i++) {
-			// skip through the cards that you know are in the game
+			// a flag that helps to skip through the cards that you know are in the game
 			boolean shouldSkip = false;
 
 			// comparing to the state's hand
@@ -61,12 +68,14 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
 					break;
 				}
 			}
+
 			// comparing to the dealer's card
 			if (i == dealerCard) {
 				shouldSkip = true;
 			}
 
-
+			// if the card is not to be skipped, then add it as child
+			// i.e. it is possible to draw it from the current hand
 			if (!shouldSkip) {
 				ArrayList<Integer> stateCards = new ArrayList<Integer>(state.cards);
 				stateCards.add(i);
@@ -79,8 +88,9 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
 
 	public BlackjackState getRandomMove(BlackjackState state) {
 		Random r = new Random();
-        
-        // biasing the algorithm so it explores the stand option more often
+
+        // biasing the random walk so it explores the stand option more often
+        // 50% of the time it will opt for standing
         if (r.nextInt(2) > 0) {
         	ArrayList<Integer> currentCards = new ArrayList<Integer>(state.cards);
         	return new BlackjackState(currentCards, true);
@@ -105,7 +115,9 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
     			if (num == dealerCard) {
     				shouldSkip = true;
     			}
-    
+    			
+    			// if the card is to be skipped, do not simulate the random walk on it
+    			// to not waste time
     			if (shouldSkip) {
     				continue;
     			} else {
@@ -113,8 +125,11 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
     				break;
     			}
             }
-    
+    		
+    		// get current hand
             ArrayList<Integer> currentCards = new ArrayList<Integer>(state.cards);
+
+            // randNum == 52 is STANDING
             if (randNum != 52) {
           		currentCards.add(randNum);
             	return new BlackjackState(currentCards, false);
@@ -124,6 +139,7 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
         }
     }
 
+    // a function that helps to find the greatest of two numbers
 	public double max(double a, double b) {
         if (a > b) {
             return a;
@@ -132,13 +148,17 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
         }
     }
 
+    // "feeding" the updates in a reward of a leaf node back to its parents
     public void backpropagate(double addedReward, BackPropType backPropType) {
         BackPropType bct = backPropType;
         while (true) {
+        	// update the parent's rewards if the current node has a parent, i.e. non-root
             if (!current.equals(root)) {
                 if (bct == BackPropType.STAND) {
                     current.parent.rewardStand += addedReward;
-                    // STAND only applies the first time
+                    // STAND are terminal - no children
+                    // so the parents of STAND nodes are by definition HITs
+                    // so update the BackPropType for the next iteration
                     bct = BackPropType.HIT;
                 } else {
                     current.parent.rewardHit+= addedReward;
@@ -146,10 +166,14 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
                 if (current.parent.count == 0) {
                     System.out.println("ISSUE WITH count = 0");
                 }
-                // count is not updated yet
+                // count is updated later, so count + 1 here
                 current.parent.reward = (current.parent.count + 1) * max(current.parent.rewardHit / current.parent.count, current.parent.rewardStand);
             }
+            // each update is +1 to the count
             current.count++;
+
+            // breaks when you reach the current game state
+            // no need to backpropagate to the root - earlier nodes will never be used
             if (current.equals(gameNode)) {
                 break;
             }
@@ -159,16 +183,16 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
 
     public Node findMax(){
 
+    	// UCB is Upper Confidence Boundary
         double ucb, maxUcb;
         ucb = 0.0;
         maxUcb = 0.0;
         Node maxNode = current.children.get(0);
 
         // for the first 52 iterations, this is basically going from 0 to 51
-        // we made changes to the classic monte carlo model because we don't it 
-        // to go sequentially and thus bias it to the first few cards, so
-        // we select randomly
-        // 52 cards - # in hand - 1 dealer card + 1 stand + 1 rollout directly from the node
+        // we made changes to the classic monte carlo model because we don't want it 
+        // to go sequentially and thus bias it to the first few cards, so we select randomly
+        // limit: 52 cards in total - # of cards in the hand - 1 dealer card + 1 from stand + 1 from rollout directly from the node
         // ^ this is very important - overflow otherwise
         if (current.count < (52 - current.state.numCards - 1 + 1 + 1)) {
             Random r = new Random();
@@ -183,12 +207,14 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
                 index++;
             }
         }
-        // if all the children have been explored at least once, then select by ucb 
+
+        // if all the children have been explored at least once, then select by UCB
         else {
             // skip the stand option
             for(int i=0; i<52; i++) {
             	Node n = current.children.get(i);
 	            if (n != null) {
+	            	// balancing exploitation (first term) and exploration (second term)
 	                ucb = n.reward / n.count + EXPLORATION * Math.sqrt(Math.log(n.parent.count)/n.count); //> CATCH COUNT COUNT == 0 AND N== 0
 	                if (ucb > maxUcb) {
 	                    maxNode = n;
@@ -200,16 +226,15 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
         return maxNode;
     }
 
+    // normalizing the scores to the interval [0,1]
     public double calcReward(BlackjackState state) {
     	double score = state.score;
     	return score / 21; 
     }
 
+    // determines if the node is terminal
     public boolean isEnd(BlackjackState state) {
     	return state.isEnd;
-    }
-
-    public static void main(String[] args){
     }
 
     // 0-51 for cards, 52 for standing
@@ -217,19 +242,27 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
     	gameNode = gameNode.children.get(newCard);
     }
 
+    // after the simulation for the current turn is done, choose the most promising option
+    // by comparing the expected value of STANDING and HITTING
     public String chooseMove() {
         double totalHitScore = 0.0;
 
+        // E(Hitting) = average of the weighted rewards from all the cards
+        // that can be dealt, excluding the dealer's cards and your current hand
         for(Node n : gameNode.children) {
             if (n != null) {
             	totalHitScore += n.reward / n.count;
             }
         }
-        totalHitScore = 1.2 * totalHitScore / 52;
+        // risk factor of 1.1
+        totalHitScore = 1.1 * totalHitScore / gameNode.children.size();
 
         double standScore;
 
-        // in other words, if the node's score is equal to 21:
+        // some gameNode's have no possible children
+        // e.g. busting
+        // so the reward is calculated from the only possible option,
+        // standing, which has the index of 52
         if (gameNode.children.size() != 0) {
         	standScore = gameNode.children.get(52).reward / gameNode.children.get(52).count;
         } else {
@@ -244,10 +277,12 @@ public class BlackjackTree extends MonteCarloTree<BlackjackState> {
     }
 
     public String play() {
+    	// run a thousand simulations
     	for (int i=0; i<1000; i++) {
     		select(gameNode);
     	}
 
+    	// choose the best move based on the simulations above
     	return chooseMove();
     }
 }
--- mcsblackjack/MonteCarloTree.java
+++ mcsblackjack/MonteCarloTree.java
@@ -6,30 +6,41 @@
 package mcsblackjack;
 import java.util.*; 
 public abstract class MonteCarloTree<State>{
+    // the higher, the more likely it is to explore different nodes
+    // the lower, the more likely it is to double down on most promising paths
     public final float EXPLORATION;
+    // number of rollouts
     public final int NUMITERATIONS;
 	public Node root;
+    // the currently simulated node;
     public Node current;
+    // where the game is at:
     public Node gameNode;
 
+    // backpropagation from either a hitting state or standing state
     enum BackPropType {
         STAND,
         HIT,
     }
 
 	public class Node{
+        // all the possible futures from the current node
         public ArrayList<Node> children;
         public Node parent;
         public boolean isChildless;
+        // computer's hand
         public State state;
-        public int action;
 
+        // how many times was the node simulated, i.e. how many times update() was called
         public long count;
+        // total reward = max(rewardHit, rewardStand)
         public double reward;
+        // reward that came from simulating hits
         public double rewardHit;
+        // reward that came from simulating stands
         public double rewardStand;
 
-
+        // custom comparison
         public boolean equals(Node a) {
             return this.state.equals(a.state);
         }
@@ -54,18 +65,19 @@ public abstract class MonteCarloTree<State>{
 
     }
 
-    abstract String chooseMove();
-
     public void select(Node n) {
         current = n;
+        // if terminal, update right away
         if (isEnd(current.state)) {
             update();
         }
-        else { 
+        else {
             if(current.isChildless){
+                // if no children AND never visited, update right away
                if (current.count==0) {
                    update();
                } else {
+                // if no children but visited once, then add children
                    State[] availMoves = findMoves(current.state);
                    for(State s : availMoves){
                        Node newNode;
@@ -84,15 +96,14 @@ public abstract class MonteCarloTree<State>{
                    update();
                 }
             } else {
-                   current = findMax();
-                   select(current);
-                   // update();
+                // if it has children, then find the child that maximizes the upper confidence bound
+               current = findMax();
+               select(current);
             }
         }
     }
 
-    abstract void update();
-
+    // complete a single random walk and return the reward once reaching a terminal node
     public double rollout(State simState){
         if(isEnd(simState)){
             return calcReward(simState);
@@ -101,12 +112,29 @@ public abstract class MonteCarloTree<State>{
         return rollout(simState);
     }
 
+    // decide the next step in the random walk
+    abstract State getRandomMove(State state);
+
+    // perform rollouts NUMITERATIONS amounts of time
+    // then backpropagate the rewards
+    abstract void update();
+
+    // feeds the updated rewards back to the node's parent chain
     abstract void backpropagate(double addedReward, BackPropType backPropType);
 
+    // finds the child that is most promising through upper confidence bound
     abstract Node findMax();
-    
-    abstract State getRandomMove(State state);
+
+    // given the rewards from the gameNode's children, decide what 
+    // course of actions maximizes the chances of winning
+    abstract String chooseMove();
+
+    // determine if the node is terminal
     abstract boolean isEnd(State state);
+
+    // find all the available futures from the given state
     abstract State[] findMoves(State state);
+
+    // calculate a reward from a terminal state
     abstract double calcReward(State state);
 }
